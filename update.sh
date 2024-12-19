2d959b0309ec537e9fbf0c50c0a1277e19857d2d
Mirai Chino <chino.mirai@jp.fujitsu.com>
2024-12-20 06:39:11 +0900

    chore

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 today=`date '+%Y%m%d'`
 echo $today
 
+rm feather/netkeiba*.feather
 echo python netkeiba.py
 python netkeiba.py
 
 rm encoder*.pickle
 rm feature*.pickle
 rm ave*.feather
+rm feather/rating*.feather
 echo python feature.py
 python feature.py
 
@@ -15,16 +17,16 @@ rm models/*.index
 echo python train.py
 python train.py
 
-rm racefeat/*.feather
-rm racefeat*.feather
-rm breakpoint*.feather
-rm bakenhit*.feather
-echo python bakenhit.py
-python bakenhit.py
+# rm racefeat/*.feather
+# rm racefeat*.feather
+# rm breakpoint*.feather
+# rm bakenhit*.feather
+# echo python bakenhit.py
+# python bakenhit.py
 
-echo docker compose -f docker-compose.deploy.yml build
-docker compose -f docker-compose.deploy.yml build
-echo docker tag lambda_arima us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
-docker tag lambda_arima us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
-echo docker push us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
-docker push us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
+# echo docker compose -f docker-compose.deploy.yml build
+# docker compose -f docker-compose.deploy.yml build
+# echo docker tag lambda_arima us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
+# docker tag lambda_arima us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
+# echo docker push us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
+# docker push us-west1-docker.pkg.dev/arima-339214/arima/lambda_arima:$today
